23109d63b39457211ac28bb2af7b19f8d7d673c7
benjamin huang <benjaminhuanghuang@gmail.com>
2022-04-08 00:10:53 -0700

    updaste

---
+++ _notes/vs-rust.md
@@ -0,0 +1,5 @@
+- Rust Leetcode Setup on VS Code [2019]
+  - https://www.youtube.com/watch?v=n1y9OSzh3lM&ab_channel=ZaneHitchcox
+
+
+  
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use leetcode::_0001_two_sum::Solution;
+
+fn main()
+{
+  assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+}
+
