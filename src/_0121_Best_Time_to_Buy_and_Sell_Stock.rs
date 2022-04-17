11e83c5e70c5da85111e8c32a14d9bf1440cfd80
benjamin huang <benjaminhuanghuang@gmail.com>
2022-04-16 23:26:23 -0700

    update

---
+++ src/_0121_Best_Time_to_Buy_and_Sell_Stock.rs
@@ -0,0 +1,31 @@
+/*
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+Easy
+*/
+pub struct Solution;
+
+
+/*
+  1. find min buy
+*/
+impl Solution {
+  pub fn max_profit(prices: Vec<i32>) -> i32 {
+    if prices.len() < 2 {
+      return 0;
+    }
+     
+    let mut max_profit: i32 = 0;
+    let mut lowest_price= i32::MAX;
+    for (_, price) in prices.iter().enumerate() {
+      if *price < lowest_price {
+        lowest_price = *price;
+      }
+      let profit = *price - lowest_price;
+      if profit > max_profit {
+        max_profit = profit;
+      }
+    }
+    max_profit
+  }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,5 @@ pub mod data_structure;
 pub mod _0001_two_sum;
 pub mod _0002_add_two_numbers;
 pub mod _0003_longest_substring_without_repeating_characters;
+pub mod _0121_Best_Time_to_Buy_and_Sell_Stock;
 
