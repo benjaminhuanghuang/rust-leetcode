f1e49cdc438434e4989f9a69ba512dea79217a11
Benjamin Huang <benjhuang@gmail.com>
2022-12-11 23:54:00 -0800

    update

--- src/solutions/_0146_lru_cache.rs
+++ src/solutions/_0146_lru_cache.rs
@@ -98,14 +98,14 @@ impl LRUCache {
 
         let ptr = if ptr.is_some() {
             // key existed: remove current entry and insert new entry
-            let ptr = ptr.unwrap().upgrade();
+            let mut ptr = ptr.unwrap().upgrade();
             self.remove(key, ptr.as_mut().unwrap());
             self.insert(key, value);
         } else {
             // key does not exist, check capacity,
             if self.length >= self.capacity {
                 // remove oldest entry and insert new entry
-                self.remove(key, self.head.as_mut().unwrap());
+                self.remove(key, self.head.take().as_mut().unwrap());
             } else {
                 self.length += 1;
             }
@@ -190,11 +190,9 @@ mod tests {
         let mut lru_cache = LRUCache::new(2);
         lru_cache.put(1, 1);
         lru_cache.put(2, 2);
-        println!("return 1");
         assert_eq!(lru_cache.get(1), 1); // returns 1
-        println!("evict key 2");
+
         lru_cache.put(3, 3); // evicts key 2
-        println!("return -1");
         assert_eq!(lru_cache.get(2), -1); // returns -1 (not found)
         lru_cache.put(4, 4); // evicts key 1
         assert_eq!(lru_cache.get(1), -1); // returns -1 (not found)
